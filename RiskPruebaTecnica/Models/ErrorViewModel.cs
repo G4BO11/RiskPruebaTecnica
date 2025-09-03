3008bc57d7f9c73306c48ff1a4f43d08b2b4617f
g4bo11 <ggutierrezortegon@gmail.com>
2025-09-03 18:37:13 -0500

    Initial Commit: Project Configuration has been established

---
+++ RiskPruebaTecnica/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RiskPruebaTecnica.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
